eb33bee35bec7156f1deb5cc84086a043da2ba0b
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-06-09 22:02:15 +0100

    fixed handleSubmit, addContact function bugs

--- src/routes/addForm.svelte
+++ src/routes/addForm.svelte
@@ -1,17 +1,54 @@
 <script>
-    import FormFields from "./formFields.svelte";
     import Button from "./button.svelte";
-    let contactInfo = [];
+    let contactInfo = [
+        {
+            firstName: "",
+            lastName: "",
+            phoneNumber: "",
+        },
+    ];
 
     const handleSubmit = () => {
-        
-    }
+        console.log(contactInfo)
+        console.log("hello")
+    };
+
+    const addMoreContact = () => {
+        contactInfo.push({
+            firstName: "",
+            lastName: "",
+            phoneNumber: "",
+        });
+
+        contactInfo = contactInfo;
+    };
 </script>
 
-<form class="my-10" on:submit|preventDefault={handleSubmit}>
-    <FormFields />
+<form class="my-10">
+    {#each contactInfo as contact, index}
+    <div class="mt-5">
+        <label for="firstName">Enter Contact First Name</label>
+        <input type="text" bind:value={contactInfo[index].firstName} class="border border-slate-400 p-3 my-2 w-full"/>
+    </div>
+    
+    <div class="mt-5">
+        <label for="lastName" >Enter Contact Last Name</label>
+        <input type="text" bind:value={contactInfo[index].lastName} class="border border-slate-400 p-3 my-2 w-full"/>
+    </div>
+    
+    <div class="mt-5">
+        <label for="phoneNumber">Enter Contact Phone Number</label>
+        <input type="text" bind:value={contactInfo[index].phoneNumber} class="border border-slate-400 p-3 my-2 w-full"/>
+    </div>
+    
+    <div class="bg-slate-300 w-full h-0.5 mt-10 rounded-md"></div>
+    {/each}
+
+
     <div class="flex items-center gap-5">
-        <i class="fa-solid fa-plus bg-slate-200 py-2 px-4 text-lg rounded-md text-slate-400 cursor-pointer" />
-        <Button btnName="Save Contact" />
+        <button type="button" on:click={addMoreContact}
+            class="fa-solid fa-plus bg-slate-200 py-2 px-4 text-lg rounded-md text-slate-400 cursor-pointer"
+        ></button>
+        <Button btnName="Save Contact" on:click={handleSubmit}/>
     </div>
 </form>
--- src/routes/button.svelte
+++ src/routes/button.svelte
@@ -2,6 +2,6 @@
     export let btnName = ""
 </script>
 
-<div class="bg-green-700 my-6 text-white p-3 rounded-md w-fit h-fit cursor-pointer">
+<button on:click class="bg-green-700 my-6 text-white p-3 rounded-md w-fit h-fit cursor-pointer">
     {btnName}
-</div>
+</button>
